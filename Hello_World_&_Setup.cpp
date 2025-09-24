bda08e37a04c6b46f2a521c20ebeabc769862ea5
BenjiMedellin <84813744+BenjiMedellin@users.noreply.github.com>
2025-09-23 19:14:27 -0500

    Create Hello_World_&_Setup.cpp
    Basically understanding the basic skeleton of a C++ program.

---
+++ Hello_World_&_Setup.cpp
@@ -0,0 +1,16 @@
+#include <iostream>                     // include input/output library.                  
+using namespace std;                    // so we can use cout, cin without std::
+
+int main() {
+  cout << "hello world" << endl;
+  return 0;                             // return 0 means program ran successfully
+}
+
+/*
+#include <iostream> : this is needed in order for output and input.
+int main() : this is where the code begins for every C++. The int means the function will 
+              return an integar. Just to confirm there is success we usually ask for a return 0.
+'<<' : this is the symbole for stream insertion and sends data to our output.
+'endl' : this is to indicate a newline or type '\n'.
+Summary : the main point is that every C++ program will have a skeleton. include -> namespace -> main() -> return 0
+*/
